f49b83f7720a346dfdbf2bbbb1ee903cb684bcc9
sihyun <sini5971@gmail.com>
2026-05-22 23:36:56 +0900

    feat: UserRepository JpaRepository 상속 추가

--- src/main/java/com/assignment/enrollment/user/repository/UserRepository.java
+++ src/main/java/com/assignment/enrollment/user/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.assignment.enrollment.user.repository;
 
-public interface UserRepository {
+import com.assignment.enrollment.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
