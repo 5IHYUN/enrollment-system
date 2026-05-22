93105a3e7ee9085dd0bd0ff031f4ebfc7f77b37b
sihyun <sini5971@gmail.com>
2026-05-22 23:50:45 +0900

    feat: CourseRepository findByStatus 추가

--- src/main/java/com/assignment/enrollment/course/repository/CourseRepository.java
+++ src/main/java/com/assignment/enrollment/course/repository/CourseRepository.java
@@ -1,4 +1,12 @@
 package com.assignment.enrollment.course.repository;
 
-public interface CourseRepository {
+import com.assignment.enrollment.course.entity.Course;
+import com.assignment.enrollment.course.entity.CourseStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByStatus(CourseStatus status);
 }
+
