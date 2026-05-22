c3cd327f04c8746ee303d6390f0142d97c68ed34
sihyun <sini5971@gmail.com>
2026-05-22 23:01:38 +0900

    feat: Course 엔티티 추가

--- src/main/java/com/assignment/enrollment/course/entity/Course.java
+++ src/main/java/com/assignment/enrollment/course/entity/Course.java
@@ -1,4 +1,50 @@
 package com.assignment.enrollment.course.entity;
 
-public class Course {
+import com.assignment.enrollment.common.entity.BaseEntity;
+import com.assignment.enrollment.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@Table(name = "classes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 생성자를 통해 값 변경 목적으로 접근하는 메시지들 차단
+public class Course extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator_id", nullable = false)
+    private User creator;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+
+    @Lob // 큰 데이터 저장
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "capacity", nullable = false)
+    private int capacity;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 20)
+    private CourseStatus status;
 }
--- src/main/java/com/assignment/enrollment/course/entity/CourseStatus.java
+++ src/main/java/com/assignment/enrollment/course/entity/CourseStatus.java
@@ -1,4 +1,7 @@
 package com.assignment.enrollment.course.entity;
 
 public enum CourseStatus {
+    DRAFT,
+    OPEN,
+    CLOSED
 }
